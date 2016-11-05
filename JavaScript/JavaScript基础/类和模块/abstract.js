007359b5649c7e632d605db9adf32437d616eb4b
xxxgitone <xxxgit@sina.com>
2016-11-05 11:56:40 +0800

    子类

---
+++ JavaScript/JavaScript基础/类和模块/abstract.js
@@ -0,0 +1,115 @@
+//定义了一个扩展函数，用来将第二个以及后续参数复制至第一个参数
+//这里处理了IE的bug：在多数IE版本中
+//如果o的属性拥有一个不可枚举的同名属性，则for/in循环
+//不会枚举对象的o的可枚举对象，也就是说，将不会正确的处理诸如toString的属性，除非我们显示检测它
+var extend = (function() {
+	//在修复它之前，检测是否含有bug
+	for (var p in {
+			toString: null
+		}) {
+		//如果代码执行到这里，那么for/in循环会正确工作并返回
+		//一个简单版本的extend函数
+		return function extend(o) {
+			for (var i = 1; i < arguments.length; i++) {
+				var source = arguments[i];
+				for (var prop in source) o[prop] = source[prop];
+			}
+			return o;
+		};
+	}
+	//如果代码执行到这里，说明for/in循环不会枚举测试对象的toString属性
+	//因此返回另一个版本的extend函数，这个函数会显示测试Object.prototype中的不可枚举的属性
+	return function patched_extend(o) {
+		for (var i = 1; i < arguments.length; i++) {
+			var source = arguments[i];
+			//复制所有属性
+			for (var prop in source) o[prop] = source[prop];
+
+			//检查特殊属性
+			for (var j = 0; j < protoprops.length; j++) {
+				prop = protoprops[j];
+				if (source.hasOwnProperty(prop))
+					o[prop] = source[prop];
+			}
+		}
+		return o;
+	};
+
+	//这个列表列出了需要检查的特殊属性
+	var protoprops = ['toString', 'valueOf', 'constructor', 'hasOwnProperty', 'isPrototypeOf', 'propertyIsEnumerable', 'toLocaleString'];
+}());
+
+/**
+ * 通过原型继承创建一个新对象，使用ECMAScript5中的Object.create方法
+ * 如果不支持，则退化使用其他方法
+ * @param  {Object} p 将要继承的对象
+ * @return Object   继承自原型对象p的属性的新对象
+ */
+function inherit(p) {
+	if (p == null) throw TypeError(); //p是一个对象，但不能为null
+	if (Object.create) //是否支持Object.create
+		return Object.create(p);
+	var t = typeof p;
+	if (t !== 'Object' && t !== 'function') throw TypeError();
+
+	function f() {}; //定义一个空函数
+	f.prototype = p; //将其原型属性设置为p
+	return new f(); //使用f()创建p的继承对象
+}
+
+//定义子类
+/**
+ * 用一个简单的函数创建简单的子类
+ * @param  {Function} superClass  父类的构造函数
+ * @param  {Object} constructor 新的子类构造函数
+ * @param  {Object} methods     实例方法：复制至原型中
+ * @param  {Object} statics     类属性：复制至构造函数中
+ * @return {[type]}             [description]
+ *
+ * B.prototype=inherit(A.prototype);
+ * B.prototype.constructor=B;
+ */
+function defineSubClass(superClass, constructor, methods, statics) {
+	//创建子类的原型对象
+	constructor.prototype = inherit(superClass.prototype);
+	constructor.prototype.constructor = constructor;
+	//像对常规类一样复制方法和属性
+	if (methods) extend(constructor.prototype, methods);
+	if (static) extend(constructor, statics);
+	return constructor;
+}
+
+//也可以通过父类构造函数的方法来做到这一点
+Function.prototype.extend = function(constructor, methods, statics) {
+	return defineSubClass(this, constructor, methods, statics);
+}
+
+//这个函数可以用作任何抽象方法，非常方便
+function abstractmethod() {
+	throw new Error('abstract method');
+}
+
+//定义了一个抽象方法contains
+function AbstractSet() {
+	throw new Error("Can't instantiate abstract classes");
+}
+AbstractSet.prototype.contains = abstractmethod;
+
+/**
+ * NotSet是AbstractSet的一个非抽象子类
+ * 所有不在其他集合中的成员都在这个集合中
+ * 因为它是在其他集合是不可写的条件下定义的
+ * 同时由于它的成员是无限个，因此它是不可枚举的
+ * 我们只能用它来检测元素成员的归属情况
+ * 这里使用Function.prototype.extend来定义这个子类
+ */
+var Noset=AbstractSet.extend(function Noset(set){this.set=set},{
+	contains:function(x){
+		return !this.set.contains(x);
+	},
+	toString:function(x){
+		return '~'+this.set.toString();
+	}
+})
+
+
---
+++ JavaScript/JavaScript基础/类和模块/subclass.js
@@ -0,0 +1,176 @@
+//定义子类
+//JavaScript的对象可以从类的原型对象中继承（通常继承的是方法）。如果哦o是类B的实例，B是A的子类，
+//那么O也一定从A继承了属性。为此首先要确保B的原型对象继承自A的原型对象
+
+// B.prototype=inherit(A.prototype);
+// B.prototype.constructor=B;
+
+/**
+ * 通过原型继承创建一个新对象，使用ECMAScript5中的Object.create方法
+ * 如果不支持，则退化使用其他方法
+ * @param  {Object} p 将要继承的对象
+ * @return Object   继承自原型对象p的属性的新对象
+ */
+function inherit(p) {
+	if (p == null) throw TypeError(); //p是一个对象，但不能为null
+	if (Object.create) //是否支持Object.create
+		return Object.create(p);
+	var t = typeof p;
+	if (t !== 'Object' && t !== 'function') throw TypeError();
+
+	function f() {}; //定义一个空函数
+	f.prototype = p; //将其原型属性设置为p
+	return new f(); //使用f()创建p的继承对象
+}
+
+function defineSubClass(superClass, constructor, methods, statics) {
+	//创建子类的原型对象
+	constructor.prototype = inherit(superClass.prototype);
+	constructor.prototype.constructor = constructor;
+	//像对常规类一样复制方法和属性
+	if (methods) extend(constructor.prototype, methods);
+	if (static) extend(constructor, statics);
+	return constructor;
+}
+
+//也可以通过父类构造函数的方法来做到这一点
+Function.prototype.extend = function(constructor, methods, statics) {
+	return defineSubClass(this, constructor, methods, statics);
+}
+
+
+function extend(o, p) {
+	for (prop in p) {
+		o[prop] = p[prop];
+	}
+	return o;
+}
+//这里展示了不使用defineSubclass函数“手动”实现子类
+//singletonSet是一个特殊的集合，它是只读的，二且包含单独的常量成员
+function SingletonSet(member) {
+	this.member = member; //记住集合中唯一的成员
+}
+
+//创建一个原型对象，这个原型对象继承自Set的原型
+SingletonSet.prototype = inherit(Set.prototype);
+
+//给原型添加属性
+//如果有同名的属性就覆盖Set.prototype中的同名属性
+extend(SingletonSet.prototype, {
+	//设置合适的constructor属性
+	constructor: SingletonSet,
+	//这个集合是只读的，所以调用add和remove都会报错
+	add: function() {
+		throw 'read-only';
+	},
+	remove: function() {
+		throw 'read-only';
+	},
+	//SingletonSet的实例总永远只有一个元素
+	size: function() {
+		return 1;
+	},
+	//这个方法只调用一次，传入这个集合的唯一成员
+	foreach: function(f, context) {
+		f.call(context, this.member);
+	},
+	//contains方法非常简单，只需检查传入的值是否匹配这个集合唯一的成员即可
+	contains: functon(x) {
+		return x === this.member;
+	}
+})
+
+//SingletonSet定义自己的equals
+SingletonSet.prototype.equals = function(that) {
+	return that instanceof Set && that.size == 1 && that.contains(member);
+}
+
+//构造函数和方法链
+
+//SingletonSet类定义了全新的集合实现，而且将它继承自其父类的核心方法全部替换
+//然而定义子类时，我们往往希望对父类的行为进行修改或者扩充
+//为了做到这一点，构造函数和子类的方法需要调用或链接到父类的构造函数和父类方法
+
+//NonNullSet是Set的子集，它不允许null和undefined作为它的成员
+
+//为了使用这种方式对成员做限制，NonNullSet需要在其add方法中对null和undefined值做检测
+//但它需要完全重新实现一个add方法，因此需要调用父类中的这个方法
+//NonNullSet构造函数同样不需要重新实现，它只需将它的参数传入父类构造函数（作为函数来调用它，而不是通过构造函数来调用）
+//通过父类的构造函数来初始化新创建的对象
+
+//在子类中调用父类的构造函数和方法
+function NonNullSet() {
+	//仅链接到父类
+	//作为普通函数调用父类的构造函数来初始化通过该构造函数调用创建对象
+	Set.apply(this, arguments);
+}
+//将NonNullSet设置成Set的子类
+NonNullSet.prototype = inherit(Set.prototype);
+NonNullSet.prototype.constructor = NonNullSet;
+
+//为了将null和undefined排除在外，只需重写add方法
+NonNullSet.prototype.add = function() {
+	//检查参数是不是null和undefined
+	for (var i = 0; i < arguments.length; i++) {
+		if (arguments[i] == null)
+			throw new Error("Can't add null or undefined to a NonNullSet");
+
+		//调用父类的add方法执行实际插入操作
+		return Set.prototype.add.apply(this, arguments);
+	}
+}
+
+//将这个非null集合的概念推而广之，称为“过滤后的集合”，这个集合中的成员必须首先传入一个过滤函数在
+//执行添加操作。
+//定义一个类工厂函数，传入一个过滤函数，返回一个新的Set类
+
+//使用方法
+//定义一个只能保存字符串的“集合”类,
+var StringSet = filteredSetSubclass(Set, function(x) {
+		return typeof x === 'string';
+	})
+	//这个集合类成员不能是null和undefined或者函数
+var MySet = filteredSetSubclass(NonNullSet, function(x) {
+	return typeof x !== 'function';
+});
+
+//类工厂和方法类
+/**
+ * 这个函数返回具体的Set类的子类
+ * 并重写该类的add方法用以添加的元素做特殊的过滤
+ */
+function filteredSetSubclass(superclass, filter) {
+	var constructor = function() { //子类构造函数
+		superclass.apply(this, arguments); //调用父类构造函数
+	};
+	var proto = constructor.prototype = inherit(superclass.prototype);
+	proto.constructor = constructor;
+	proto.add = function() {
+		for (var i = 0; i < arguments.length; i++) {
+			var v = arguments[i];
+			if (!filter(v)) throw ("value" + v + " rejected by filter");
+		}
+		//调用父类的add方法
+		superclass.prototype.add.apply(this, arguments);
+	}
+	return constructor;
+}
+
+//使用包装函数和Function.prototype.extend方法来冲重写NonNullSet
+var NonNullSet = (functon() {
+			var superclass = Set; //定义并立即调用这个函数
+			return superclass.extend(function() {
+					superclass.apply(this, arguments);
+				}, {
+					add: function() {
+						for (var i = 0; i < arguments.length; i++) {
+							if (arguments[i] == null)
+								throw new Error("Can't add null or undefined to a NonNullSet");
+
+							//调用父类的add方法执行实际插入操作
+							return Set.prototype.add.apply(this, arguments);
+						}
+					});
+			}());
+
+//组合vs子类
