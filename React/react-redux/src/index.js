870a3791a5a5fe13afdd76a3e3cc0dc685610246
xxxgitone <xxxgit@sina.com>
2016-12-28 21:09:03 +0800

    react-redux 实例

--- React/react-redux/src/index.js
+++ React/react-redux/src/index.js
@@ -1,66 +1,12 @@
-import { applyMiddleware, createStore } from 'redux';
-import axios from 'axios';
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
-import promise from 'redux-promise-middleware';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
-const initalState = {
-	fetching: false,
-	fetched: false,
-	users: [],
-	error: null
-}
-const reducer = (state=initalState, action) =>{
-	switch (action.type){
-		case "FETCH_USERS_PENDING": {
-			return {
-				...state, 
-				fetching: true
-			}
-			break;
-		}
-		case "FETCH_USERS_REJECTED": {
-			return {
-				...state, 
-				fetching: false, 
-				error: action.payload
-			}
-			break;
-		}
-		case "FETCH_USERS_FULFILLED": {
-			return {
-				...state, 
-				fetching: false, 
-				fetched: true, 
-				users: action.payload
-			}
-			break;
-		}
-	}
-	return state;
-}
+import Layout from './components/Layout';
+import store from './store';
 
+const rootEl = document.getElementById('root');
 
-const middleware = applyMiddleware(promise(), thunk, logger());
-
-const store = createStore(reducer, middleware);
-
-
-store.dispatch({
-	type: 'FETCH_USERS',
-	payload: axios.get("https://rest.learncode.academy/api/wstern/users")
-})
-
-//下面这些异步可以使用promise代替
-// store.dispatch((dispatch) => {
-// 	dispatch({type: 'FETCH_USERS_START'});
-// 	axios.get("http://rest.learncode.academy/api/wstern/users")
-// 		.then((response) =>{
-// 			dispatch({type: 'RECEICE_USERS', payload: response.data})
-// 		})
-// 		.catch((err) =>{
-// 			dispatch({type: 'FETCH_USERS_ERROR',payload: err})
-// 		})
-// });
-
-
+ReactDOM.render(<Provider store={store}>
+  <App />
+</Provider>, rootEl);
