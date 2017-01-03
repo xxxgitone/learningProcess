376107800fec4463d7846b85ad2c2fce7b647928
xxxgitone <xxxgit@sina.com>
2017-01-03 14:44:07 +0800

    深入浅出node.js

---
+++ Node/diveintonode/02/module_path.js
@@ -0,0 +1,9 @@
+console.log(module.paths);
+
+// [ 'F:\\FrontEndWork\\learningProcess\\Node\\diveintonode\\02\\node_modules',
+//   'F:\\FrontEndWork\\learningProcess\\Node\\diveintonode\\node_modules',
+//   'F:\\FrontEndWork\\learningProcess\\Node\\node_modules',
+//   'F:\\FrontEndWork\\learningProcess\\node_modules',
+//   'F:\\FrontEndWork\\node_modules',
+//   'F:\\node_modules' ]
+//   逐级往上找
