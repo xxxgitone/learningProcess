0206047872a6efd45cb5bed3e84b9d35709966de
xxxgitone <xxxgit@sina.com>
2017-02-02 09:41:25 +0800

    day20 :weary:

---
+++ JavaScript/javascript30/20 - Speech Detection/index.js
@@ -0,0 +1,14 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement('p');
+const words = document.querySelector('.words');
+words.appendChild(p);
+
+recognition.addEventListener('result', e => {
+	console.log(e)
+});
+
+recognition.start();
