fb36a24dc8f8db0b94e2cc16c39f908565172804
xxxgitone <xxxgit@sina.com>
2017-02-09 22:39:26 +0800

    day27 :stuck_out_tongue:

---
+++ JavaScript/javascript30/27 - Click and Drag/scripts.js
@@ -0,0 +1,36 @@
+const slider = document.querySelector('.items');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    //e.pageX鼠标指针的位置，相对于文档的左边缘
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+
+});
+
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+
+slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 3;
+    slider.scrollLeft = scrollLeft - walk;
+
+});
+
+
+
+
