fa7227f2c5cd04fa595d81046d76fcc7b689a487
xxxgitone <xxxgit@sina.com>
2017-02-11 13:52:55 +0800

    day28 :smiley:

---
+++ JavaScript/javascript30/29 - Countdown Timer/scripts.js
@@ -0,0 +1,59 @@
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
+
+function timer(seconds) {
+	clearInterval(countdown);
+
+	const now = Date.now();
+	const then = now + seconds * 1000;
+	displayTimeLeft(seconds);
+	displayEndTimer(then);
+
+	countdown = setInterval(() => {
+		const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+		if (secondsLeft <= 0) {
+			clearInterval(countdown);
+			return;
+		}
+
+		displayTimeLeft(secondsLeft);
+	}, 1000);
+}
+
+function displayTimeLeft(seconds) {
+	const minutes = Math.floor(seconds / 60);
+	const remainderSeconds = seconds % 60;
+	const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+	document.title = display;
+	timerDisplay.textContent = display;
+}
+
+function displayEndTimer(timestamp) {
+	const end = new Date(timestamp);
+	const hour = end.getHours();
+	const minutes = end.getMinutes();
+	const adjustedHour = hour > 12 ? hour - 12 : hour;
+	endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+}
+
+function startTimer() {
+	const seconds = parseInt(this.dataset.time);
+	timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function (e) {
+	e.preventDefault();
+	const mins = this.minutes.value;
+	timer(mins * 60);
+	this.reset();
+})
+
+
+
+
+
+
