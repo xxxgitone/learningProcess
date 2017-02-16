ab262824af54bc91d6115222aa79706d14b27ef0
xxxgitone <xxxgit@sina.com>
2017-02-16 18:47:53 +0800

    表单控件

---
+++ Vue/vue-intro/forms.js
@@ -0,0 +1,39 @@
+var example1 = new Vue({
+	el: '#example1',
+	data: {
+		message: ''
+	}
+})
+
+var example2 = new Vue({
+	el: '#example2',
+	data: {
+		checked: false
+	}
+})
+
+var example3 = new Vue({
+	el: '#example3',
+	data: {
+		checkedNames: []
+	}
+})
+
+var example4 = new Vue({
+	el: '#example4',
+	data: {
+		selected: ''
+	}
+})
+
+var example5 = new Vue({
+	el: '#example5',
+	data: {
+		selected: 'A',
+	    options: [
+	      { text: 'One', value: 'A' },
+	      { text: 'Two', value: 'B' },
+	      { text: 'Three', value: 'C' }
+	    ]
+	}
+})
