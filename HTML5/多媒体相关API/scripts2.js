416f81ca6393e1615c42fc464441aee04eed7f70
xxxgitone <xxxgit@sina.com>
2017-02-17 21:46:36 +0800

    事件捕捉

--- HTML5/多媒体相关API/scripts2.js
+++ HTML5/多媒体相关API/scripts2.js
@@ -1,9 +1,22 @@
 function playOrPauseVideo() {
-	const videoUrl = document.querySelector('#videoUrl').value;
+	let videoUrl = document.querySelector('#valueUrl').value;
 	const video = document.querySelector('#video');
+	const playButton = document.querySelector('#playButton');
 
 	//使用事件监听方式捕捉事件
 	video.addEventListener('timeupdate', () => {
 		const timeDisplay = document.querySelector('#time');
-	})
+		//用秒数来显示当前播放进度
+		currentTime = Math.floor(video.currentTime);
+		duration = Math.floor(video.duration);
+		timeDisplay.innerHTML = `${currentTime} / ${duration} (秒)`;
+	});
+
+	if (video.paused) {
+		video.play();
+		playButton.textContent = '播放';
+	} else {
+		video.pause();
+		playButton.textContent = '暂停';
+	}
 }
