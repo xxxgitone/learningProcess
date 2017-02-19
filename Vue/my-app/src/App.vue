f08642c49537e1fc02b5c7a71701afa2df928750
xxxgitone <xxxgit@sina.com>
2017-02-19 11:07:12 +0800

    vue-cli

---
+++ Vue/my-app/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+
+    <message>Hello There</message>
+
+    <message>Hello There Again</message>
+
+    <ul>
+      
+      <li v-for="info in infos" v-text="info"></li>
+
+    </ul>
+
+
+  </div>
+</template>
+
+<script>
+import Message from './components/Message.vue';
+import axios from 'axios';
+import Vue from 'vue';
+
+Vue.prototype.$http =  axios;
+
+export default {
+  name: 'app',
+
+  components: { Message },
+
+  data() {
+    
+    return {
+      infos: []
+    }
+
+  },
+
+  mounted() {
+
+    // axios.get('https://api.github.com/users/xxxgitone')
+      // .then(response => this.infos = response.data)
+    
+    this.$http.get('https://api.github.com/users/xxxgitone')
+      .then(response => this.infos = response.data)
+
+  }
+
+
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ Vue/my-app/src/components/Message.vue
@@ -0,0 +1,30 @@
+<template>
+	
+	<div class="box">
+		<p>
+			<slot><slot>
+		</p>
+	</div>
+
+</template>
+
+
+<script>
+
+export default {
+
+}
+
+</script>
+
+
+
+<style>
+	.box {
+		background-color: #e3e3e3;
+		padding: 10px;
+		border: 1px solid #c5c5c5;
+		margin-bottom: 1em;
+	}
+
+</style>
