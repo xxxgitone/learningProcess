7583d9f73ec957e2811f26cf5edd7708ce904660
xxxgitone <xxxgit@sina.com>
2017-04-20 19:04:38 +0800

    中间件验证购物车

---
+++ Node/node-express/lib/cartValidation.js
@@ -0,0 +1,23 @@
+module.exports = {
+    checkWaivers: function(req, res, next){
+		var cart = req.session.cart;
+		if(!cart) return next();
+        //some方法测试数组中的某些元素是否通过了指定函数的测试
+		if(cart.some(function(item){ return item.product.requiresWaiver; })){
+			if(!cart.warnings) cart.warnings = [];
+			cart.warnings.push('One or more of your selected tours requires a waiver.');
+		}
+		next();
+	},
+
+	checkGuestCounts: function(req, res, next){
+		var cart = req.session.cart;
+		if(!cart) return next();
+		if(cart.some(function(item){ return item.guests > item.product.maximumGuests; })){
+			if(!cart.errors) cart.errors = [];
+			cart.errors.push('One or more of your selected tours cannot accommodate the ' +
+				'number of guests you have selected.');
+		}
+		next();
+	}
+}
