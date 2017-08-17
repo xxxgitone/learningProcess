7a7870f6d3a76b2fc38965998f5ca3308c3c6803
xxxgitone <xxxgit@sina.com>
2017-08-17 23:54:30 +0800

    注册登录

--- React/reactdemo/demo10/src/js/components/mobile-header.js
+++ React/reactdemo/demo10/src/js/components/mobile-header.js
@@ -1,14 +1,142 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router'
+import {Row, Col} from 'antd'
+import { 
+  Menu, 
+  Icon,
+  Tabs,
+  message,
+  Form,
+  Input,
+  Button,
+  Checkbox,
+  Modal
+} from 'antd'
 
-export default class MobileHeader extends Component{
+const FormItem = Form.Item
+const SubMenu = Menu.SubMenu
+const MenuItemGroup = Menu.ItemGroup
+const TabPane = Tabs.TabPane
+
+class MobileHeader extends Component{
+  constructor (props) {
+    super(props)
+    this.state = {
+      current: 'top',
+      modalVisible: false,
+      action: 'login',
+      hasLogined: false,
+      userNickName: '',
+      userid: 0
+    }
+    this.setModalVisible = this.setModalVisible.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.login = this.login.bind(this)
+    this.callback = this.callback.bind(this)
+  }
+
+  setModalVisible (value) {
+    this.setState({modalVisible: value})
+  }
+
+  handleClick (e) {
+    if (e.key === 'register') {
+      this.setState({current: 'register'})
+      this.setModalVisible(true)
+    } else {
+      this.setState({current: e.key})
+    }
+  }
+
+  login () {
+    this.setModalVisible(true)
+  }
+
+  callback(key) {
+		if (key == 1) {
+			this.setState({action: 'login'});
+		} else if (key == 2) {
+			this.setState({action: 'register'});
+		}
+	}
+
+  handleSubmit (e) {
+    e.preventDefault()
+    const myFetchOptions = {
+      method: 'GET'
+    }
+    const formData = this.props.form.getFieldsValue()
+    fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=" + this.state.action
+		+ "&username="+formData.userName+"&password="+formData.password
+		+"&r_userName=" + formData.r_userName + "&r_password="
+		+ formData.r_password + "&r_confirmPassword="
+		+ formData.r_confirmPassword, myFetchOptions)
+		.then(response => response.json())
+		.then(json => {
+			this.setState({userNickName: json.NickUserName, userid: json.UserId})
+		})
+		if (this.state.action=="login") {
+			this.setState({hasLogined:true})
+		}
+		message.success("请求成功！")
+		this.setModalVisible(false)
+  }
+
   render(){
+    const {getFieldProps} = this.props.form
+    const userShow = this.state.hasLogined 
+     ? <Link>
+         <Icon type="inbox"/>
+       </Link>
+     : <Icon type="setting" onClick={this.login}/>
     return (
       <div id="mobileheader">
         <header>
           <img src="./src/images/logo.png" alt="logo"/>
           <span>ReactNews</span>
+          {userShow}
         </header>
+
+        <Modal 
+          title="用户中心" 
+          wrapClassName="vertical-center-modal" 
+          visible={this.state.modalVisible} 
+          onCancel={() => this.setModalVisible(false)} 
+          onOk={() => this.setModalVisible(false)} 
+          okText="关闭"
+        >
+          <Tabs type="card">
+            <TabPane tab="登录" key="1">
+              <Form horizontal onSubmit={this.handleSubmit}>
+                <FormItem label="账户">
+                  <Input placeholder="请输入您的账号" {...getFieldProps('userName')}/>
+                </FormItem>
+                <FormItem label="密码">
+                  <Input type="password" placeholder="请输入您的密码" {...getFieldProps('password')}/>
+                </FormItem>
+                <Button type="primary" htmlType="submit">登录</Button>
+              </Form>
+            </TabPane>
+            <TabPane tab="注册" key="2">
+              <Form horizontal onSubmit={this.handleSubmit}>
+                <FormItem label="账户">
+                  <Input placeholder="请输入您的账号" {...getFieldProps('r_userName')} />
+                </FormItem>
+                <FormItem label="密码">
+                  <Input type="password" placeholder="请输入您的密码" {...getFieldProps('r_password')} />
+                </FormItem>
+                <FormItem label="确认密码">
+                  <Input type="password" placeholder="请再次输入您的密码" {...getFieldProps('r_confirmPassword')} />
+                </FormItem>
+                <Button type="primary" htmlType="submit">注册</Button>
+              </Form>
+            </TabPane>
+          </Tabs>
+        </Modal>
       </div>
     )
   }
 }
+
+export default MobileHeader = Form.create({})(MobileHeader)
--- React/reactdemo/demo10/src/js/components/pc-header.js
+++ React/reactdemo/demo10/src/js/components/pc-header.js
@@ -1,16 +1,100 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router'
 import {Row, Col} from 'antd'
-import { Menu, Icon } from 'antd';
+import { 
+  Menu, 
+  Icon,
+  Tabs,
+  message,
+  Form,
+  Input,
+  Button,
+  Checkbox,
+  Modal
+} from 'antd'
 
-export default class PCHeader extends Component{
-  constructor () {
-    super()
+const FormItem = Form.Item
+const SubMenu = Menu.SubMenu
+const MenuItemGroup = Menu.ItemGroup
+const TabPane = Tabs.TabPane
+
+class PCHeader extends Component{
+  constructor (props) {
+    super(props)
     this.state = {
-      current: 'top'
+      current: 'top',
+      modalVisible: false,
+      action: 'login',
+      hasLogined: false,
+      userNickName: '',
+      userid: 0
+    }
+    this.setModalVisible = this.setModalVisible.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.callback = this.callback.bind(this)
+  }
+
+  setModalVisible (value) {
+    this.setState({modalVisible: value})
+  }
+
+  handleClick (e) {
+    if (e.key === 'register') {
+      this.setState({current: 'register'})
+      this.setModalVisible(true)
+    } else {
+      this.setState({current: e.key})
     }
   }
 
+  callback(key) {
+		if (key == 1) {
+			this.setState({action: 'login'});
+		} else if (key == 2) {
+			this.setState({action: 'register'});
+		}
+	}
+
+  handleSubmit (e) {
+    e.preventDefault()
+    const myFetchOptions = {
+      method: 'GET'
+    }
+    const formData = this.props.form.getFieldsValue()
+    fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=" + this.state.action
+		+ "&username="+formData.userName+"&password="+formData.password
+		+"&r_userName=" + formData.r_userName + "&r_password="
+		+ formData.r_password + "&r_confirmPassword="
+		+ formData.r_confirmPassword, myFetchOptions)
+		.then(response => response.json())
+		.then(json => {
+			this.setState({userNickName: json.NickUserName, userid: json.UserId});
+		})
+		if (this.state.action=="login") {
+			this.setState({hasLogined:true})
+		}
+		message.success("请求成功！")
+		this.setModalVisible(false)
+  }
+
   render(){
+    const {getFieldProps} = this.props.form
+    const userShow = this.state.hasLogined 
+      ? <Menu.Item key="logout" className="register">
+          <Button type="primary" htmlType="button">     
+            {this.state.userNickName}
+          </Button>
+          &nbsp;&nbsp;
+          <Link target="_blank">
+            <Button type="dashed" htmlType="button">个人中心</Button>
+          </Link>
+          &nbsp;&nbsp;
+          <Button type="ghost" htmlType="button">退出</Button>
+        </Menu.Item>
+      : <Menu.Item key="register" className="register">
+          <Icon type="appstore"/>注册/登录
+        </Menu.Item>
     return (
       <header>
         <Row>
@@ -22,7 +106,7 @@ export default class PCHeader extends Component{
             </a>
           </Col>
           <Col span={16}>
-            <Menu mode="horizontal" selectedKeys={[this.state.current]}>
+            <Menu mode="horizontal" onClick={this.handleClick} selectedKeys={[this.state.current]}>
               <Menu.Item key="top">
                 <Icon type="appstore" />头条
               </Menu.Item>
@@ -47,7 +131,45 @@ export default class PCHeader extends Component{
               <Menu.Item key="shishang">
                 <Icon type="appstore" />时尚
               </Menu.Item>
+              {userShow}
             </Menu>
+
+            <Modal 
+              title="用户中心" 
+              wrapClassName="vertical-center-modal" 
+              visible={this.state.modalVisible} 
+              onCancel={() => this.setModalVisible(false)} 
+              onOk={() => this.setModalVisible(false)} 
+              okText="关闭"
+            >
+              <Tabs type="card" onChange={this.callback}>
+                <TabPane tab="登录" key="1">
+									<Form horizontal onSubmit={this.handleSubmit}>
+										<FormItem label="账户">
+											<Input placeholder="请输入您的账号" {...getFieldProps('userName')}/>
+										</FormItem>
+										<FormItem label="密码">
+											<Input type="password" placeholder="请输入您的密码" {...getFieldProps('password')}/>
+										</FormItem>
+										<Button type="primary" htmlType="submit">登录</Button>
+									</Form>
+								</TabPane>
+                <TabPane tab="注册" key="2">
+                  <Form horizontal onSubmit={this.handleSubmit}>
+                    <FormItem label="账户">
+                      <Input placeholder="请输入您的账号" {...getFieldProps('r_userName')} />
+                    </FormItem>
+                    <FormItem label="密码">
+                      <Input type="password" placeholder="请输入您的密码" {...getFieldProps('r_password')} />
+                    </FormItem>
+                    <FormItem label="确认密码">
+                      <Input type="password" placeholder="请再次输入您的密码" {...getFieldProps('r_confirmPassword')} />
+                    </FormItem>
+                    <Button type="primary" htmlType="submit">注册</Button>
+                  </Form>
+                </TabPane>
+              </Tabs>
+            </Modal>
           </Col>
           <Col span={2}></Col>
         </Row>
@@ -55,3 +177,5 @@ export default class PCHeader extends Component{
     )
   }
 }
+
+export default PCHeader = Form.create({})(PCHeader)
