79bd2e324e82437675e66a543dbe86bd6616013d
xxxgitone <xxxgit@sina.com>
2017-09-04 20:59:08 +0800

    jsonp

---
+++ JavaScript/JavaScript基础/jsonp/jsonp.js
@@ -0,0 +1,50 @@
+/**
+ * 获取随机字符串,用于拼接
+ * @param {string} prefix [前导名字]
+ * @param {number} num  [字符串长度]
+ */
+function getRandomName (prefix, num) {
+	return prefix + Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, num)
+}
+
+/**
+ * 创建script标签
+ * @param {请求路径} url 
+ */
+function createScript (url) {
+	const script = document.createElement('script')
+	script.setAttribute('type', 'text/javascript')
+	script.setAttribute('src', url)
+	script.async = true
+	return script
+}
+
+/**
+ * 实现请求
+ * @param {路径} url 
+ */
+function jsonp (url) {
+	return new Promise((resolve, reject) => {
+		const cbName = getRandomName('callback')
+		window[cbName] = function (data) {
+			resolve(data)
+		}
+
+		url += url.indexOf('?') > -1 ? '&' : '?'
+		const script = createScript(`${url}callback=${cbName}`)
+
+		script.onload = function () {
+			script.onload = null
+			if (script.parentNode) {
+				script.parentNode.removeChild(script)
+			}
+			window[cbName] = null
+		}
+
+		script.onerror = function () {
+			reject()
+		}
+
+		document.getElementsByTagName('head')[0].appendChild(script)
+	})
+}
