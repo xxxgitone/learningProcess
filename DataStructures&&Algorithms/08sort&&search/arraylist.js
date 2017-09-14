7a8cf51255f0ef98fb8b6d31010afc48381067ca
xxxgitone <xxxgit@sina.com>
2017-09-14 20:46:07 +0800

    快速排序

--- DataStructures&&Algorithms/08sort&&search/arraylist.js
+++ DataStructures&&Algorithms/08sort&&search/arraylist.js
@@ -118,6 +118,62 @@ function ArrayList () {
     this.mergeSort = function () {
         array = mergeSortRec(array)
     }
+
+    this.quickSort = function () {
+        quick(array, 0, array.length -1)
+    }
+    /**
+     * 快速排序思想
+     * 1） 从数组中选择中间一项作为主元
+     * 2） 创建两个指针，左边一个指向数组第一项，右边一个指向数组最后一项。移动左指针直到找到一个比主元大的元素，接着，移动       右指针直到找到一个比主元小的元素，然后交换他们，重复这个过程，直到左指针超过了右指针。这个过程将使得比主元小的值
+     *      都在主元之前，大的都在主元之后。这个过程叫做划分操作
+     * 3） 接着，算法对划分操作后的小数组（较主元小的值组成的子数组，以及较主元大的值组成的子数组）重复之前的两个步骤，直到      数组已完全排序
+     * @param {待排序数组} array 
+     * @param {左边划分} left 
+     * @param {右边划分} right 
+     */
+    function quick (array, left, right) {
+        var index
+
+        if (array.length > 1) {
+            index = partition(array, left, right)
+
+            if (left < index -1) {
+                quick(array, left, index -1)
+            } 
+
+            if (index < right) {
+                quick(array, index, right)
+            }
+        }
+    }
+
+    function partition (array, left, right) {
+        var pivot = array[Math.floor((right + left) / 2)],
+            i = left,
+            j = right
+        
+        while (i <= j) {
+            while (array[i] < pivot) {
+                i++
+            }
+            while (array[j] > pivot) {
+                j--
+            }
+            if (i <= j) {
+                swapQuickStort(array, i, j)
+                i++
+                j--
+            }
+        }
+        return i
+    }
+
+    function swapQuickStort (array, index1, index2) {
+        var aux = array[index1]
+        array[index1] = array[index2]
+        array[index2] = aux
+    }
 }
 
 function createNonSortedArray (size) {
@@ -140,8 +196,15 @@ function createNonSortedArray (size) {
 // console.log(array.toString()) // 1,2,3,4,5
 
 // 插入
+// var array = createNonSortedArray(5)
+// console.log(array.toString()) // 5,4,3,2,1
+// array.insertionSort()
+// console.log(array.toString()) // 1,2,3,4,5
+
+// 快速
 var array = createNonSortedArray(5)
 console.log(array.toString()) // 5,4,3,2,1
-array.insertionSort()
+array.quickSort()
 console.log(array.toString()) // 1,2,3,4,5
+
 
