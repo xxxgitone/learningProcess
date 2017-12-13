3e17b87ce5a6dee08bbd1d20387c2119c4089332
xxxgitone <xxxgit@sina.com>
2017-12-13 23:21:51 +0800

    第一版函数

--- someExamples/currying/index.js
+++ someExamples/currying/index.js
@@ -11,4 +11,24 @@
 // var addCurry = curry(add)
 // addCurry(1)(3) // 3
 
-// TODO：看了原理，明天实现
+// TODO
+var curry = function (fn) {
+  var ags = [].slice.call(arguments, 1)
+  return function() {
+    var newArgs = args.concat([].slice.call(arguments))
+    return fn.apply(this, newArgs)
+  }
+}
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// var addCurry = curry(add, 1, 2);
+// addCurry() // 3
+// //或者
+// var addCurry = curry(add, 1);
+// addCurry(2) // 3
+// //或者
+// var addCurry = curry(add);
+// addCurry(1, 2) // 3
